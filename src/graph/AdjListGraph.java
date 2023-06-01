5e5a51fb4da96298b6f55d5cfb0ef6228d6e819f
Michael Magdy <mmagdy.eg@solutions.com.sa>
2023-06-01 15:19:36 +0300

    Until video 173 : Number of Islands | Leetcode #200 | Animation | Amazon Interview Question

--- src/graph/AdjListGraph.java
+++ src/graph/AdjListGraph.java
@@ -76,6 +76,49 @@ public class AdjListGraph {
         System.out.println();
     }
 
+    public void dfs() {
+        boolean[] visited = new boolean[V];
+        for (int v = 0; v < V; v++) {
+            if (!visited[v]) {
+                dfs(v, visited);
+            }
+        }
+    }
+
+    private void dfs(int v, boolean[] visited) {
+        visited[v] = true;
+        System.out.print(v + " ");
+        for (int w : adj[v]) {
+            if (!visited[w]) {
+                dfs(w, visited);
+            }
+        }
+    }
+
+    //return the count of connected nodes in a graph
+    public int countConnectedNodes(){
+        boolean[] visited = new boolean[V];
+        int[] compId = new int[V];
+        int count = 0;
+        for (int v = 0; v < V; v++) {
+            if (!visited[v]) {
+                dfs(v, visited, compId, count);
+                count++;
+            }
+        }
+        return count;
+    }
+
+    private void dfs(int v, boolean[] visited, int[] compId, int count) {
+        visited[v] = true;
+        compId[v] = count;
+        for (int w : adj[v]) {
+            if (!visited[w]) {
+                dfs(w, visited, compId, count);
+            }
+        }
+    }
+
     public static void main(String[] args) {
         AdjListGraph alg = new AdjListGraph(5);
         alg.addEdge(0, 1);
@@ -86,5 +129,10 @@ public class AdjListGraph {
         System.out.println(alg);
         alg.bfs(0);
         alg.dfs(0);
+        System.out.println("recursive dfs");
+        alg.dfs();
+        System.out.println();
+        System.out.println("connected nodes or vertices");
+        System.out.println(alg.countConnectedNodes());
     }
 }
