9b41e2680b3edae9acbe2a2633de4cb503b3bedf
Michael Magdy <mmagdy.eg@solutions.com.sa>
2023-06-17 16:48:56 +0300

    Until video 190 : Top Down Approach - Fibonacci Number in Dynamic Programming | Implementation

--- src/dynamicProgramming/Fibonacci.java
+++ src/dynamicProgramming/Fibonacci.java
@@ -16,7 +16,7 @@ public class Fibonacci {
         return left + right;
     }
 
-    //calculate direct fibonacci number using dynamic programming
+    //calculate direct fibonacci number using dynamic programming up-bottom approach
     public int fibDP(int index){
 
         int[] table = new int[index+1];  //0 -> n
@@ -28,8 +28,28 @@ public class Fibonacci {
         return table[index];
     }
 
+    //calculate direct fibonacci number using dynamic programming top-down approach
+    public int fibDP(int[] memo, int index){  //array size should be index + 1
+
+        if (index < 0){
+            throw new  IllegalArgumentException("Invalid input");
+        }
+
+        if (memo[index] == 0){
+            if (index < 2){
+                memo[index] = index;
+            } else {
+                int left = fibDP(memo, index-1);
+                int right = fibDP(memo, index-2);
+                memo[index] = left + right;
+            }
+        }
+        return memo[index];
+    }
+
     public static void main(String[] args) {
         Fibonacci fibonacci = new Fibonacci();
         System.out.println("The 6th fibonacci number is = " + fibonacci.fibDP(6));
+        System.out.println("The 6th fibonacci number is = " + fibonacci.fibDP(new int[6+1],6));
     }
 }
